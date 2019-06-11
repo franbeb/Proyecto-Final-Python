46d2f504e22b18bac2963bac340803dc96e5369d
nazamoresco <50846285+nazamoresco@users.noreply.github.com>
2019-06-11 09:30:16 -0300

    Add files via upload
    -Agrega parte de configuracion y extension de constantes

---
+++ Proyecto-Final-Python-master/modulos/juego.py
@@ -0,0 +1,8 @@
+#codigo de la sopa de letras
+def click():
+def selcionar_tipodepal():#verbo/sustantivo/adjetivo
+def checkear():
+def generar_tablero():
+def ():
+def ():
+def main(): #empieza a jugar
